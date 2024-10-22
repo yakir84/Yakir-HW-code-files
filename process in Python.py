461d00b193906259d21ae0250876ff3d35f9af33
Yakir zuberi <yakir7784@gmail.com>
2024-10-22 14:58:33 +0300

    Changing input in the code

--- process in Python.py
+++ process in Python.py
@@ -1,25 +1,27 @@
 import os
 import multiprocessing
 
+
 def print_numbers(n):
     for i in range(n + 1):
         print(i)
 
 
-def secondary(name):
+def secondary(name, number):
+    
     print("the name is " + name) 
     print("The secondary PID is " + str(os.getpid()) + " and secondary parent PID is " + str(os.getppid()))
+    print_numbers(number)
+
 
 def main():
     user_input = int(input("Enter a number: "))
-    print_numbers(user_input)
     print("The main PID is " + str(os.getpid()) + " and main parent PID is " + str(os.getppid()))
-    process = multiprocessing.Process(target=secondary, args=("yakir",))
+    process = multiprocessing.Process(target=secondary, args=("yakir", user_input))
     process.start()
     process.join()
 
 
-
-
 if __name__ == "__main__":
     main()
+
